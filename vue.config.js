dad517dc1cc9c6cf86a0a5a2d79eaf31a50aac11
LeeSeHee <euc728@naver.com>
2022-02-16 15:26:14 +0900

    config 수정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 // vue.config.js
 module.exports = {
     publicPath: '/portfolio_pb',
-    outputDir: './docs'
+    outputDir: './docs',
+    devServer: {
+        historyApiFallback: true,
+    },
 }
